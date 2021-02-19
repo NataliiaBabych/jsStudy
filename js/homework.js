850da4f32bfb29e050f3b49334ce17ef351fc425
Nataliia Babych <babicka89@gmail.com>
2021-02-19 16:31:22 +0200

    hw

---
+++ js/homework.js
@@ -0,0 +1,22 @@
+// HW
+// Module 2 
+// ex9
+
+// function getExtremeElements(array) {
+//   let firstAndLast = [array[0], array[array.length - 1]];
+//   return firstAndLast;
+//   }
+
+// console.log(getExtremeElements(['яблоко', 'персик', 'груша', 'банан']));
+
+// ex10
+
+// ex11
+
+// ex12
+
+// ex13
+
+// ex14
+
+// ex15
